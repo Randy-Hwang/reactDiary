f3be566ae5b047f12f82b2fce4931e318ef68a8b
Randy <99023957+Randy-Hwang@users.noreply.github.com>
2022-06-09 20:43:13 +0900

    react diary remaked version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
+import Home from "./pages/Home";
+import New from "./pages/New";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h2>Hi!</h2>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/diary" element={<Diary />} />
+          <Route path="/edit" element={<Edit />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
