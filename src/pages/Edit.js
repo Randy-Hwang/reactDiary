80f3d437f52acc7e877a4c72a304984c16c93a19
Randy <99023957+Randy-Hwang@users.noreply.github.com>
2022-06-12 12:23:34 +0900

    add buttons

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,29 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const id = searchParams.get("id");
+
   return (
     <div>
       <h1>Edit</h1>
       <p>This is Edit</p>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Home으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        Go Back
+      </button>
     </div>
   );
 };
