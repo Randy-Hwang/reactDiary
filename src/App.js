76612204042ae9a757f792862ea70d2a8454acc4
Randy <99023957+Randy-Hwang@users.noreply.github.com>
2022-06-13 11:34:33 +0900

    cleaned

--- src/App.js
+++ src/App.js
@@ -7,54 +7,10 @@ import Edit from "./pages/Edit";
 import Home from "./pages/Home";
 import New from "./pages/New";
 
-// COMPONENTS
-import MyButton from "./components/MyButton";
-import MyHeader from "./components/MyHeader";
-
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <MyHeader
-          headText={"APP"}
-          leftChild={
-            <MyButton
-              text={"left"}
-              onClick={() => console.log("left clicked")}
-            />
-          }
-          rightChild={
-            <MyButton
-              text={"right"}
-              onClick={() => console.log("right clicked")}
-            />
-          }
-        />
-
-        <h2>Hi!</h2>
-        {/* public 폴더로 바로 접근할 수 있게 해주는 process.env.PUBLIC_URL  */}
-        {/* <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} /> */}
-
-        <MyButton
-          text={"Save"}
-          onClick={() => console.log("Button clicked")}
-          type={"positive"}
-        />
-        <MyButton
-          text={"Edit"}
-          onClick={() => console.log("Button clicked")}
-          type={"default"}
-        />
-        <MyButton
-          text={"Delete"}
-          onClick={() => console.log("Button clicked")}
-          type={"negative"}
-        />
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
