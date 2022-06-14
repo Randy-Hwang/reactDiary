297ec4293cdc5f5dc8bf5f794c9ea51631920eb4
Randy <99023957+Randy-Hwang@users.noreply.github.com>
2022-06-14 16:03:36 +0900

    add Write New Diary function

---
+++ src/components/EmotionItem.js
@@ -0,0 +1,22 @@
+const EmotionItem = ({
+  emotion_id,
+  emotion_descript,
+  emotion_img,
+  onClick,
+  isSelected,
+}) => {
+  return (
+    <div
+      onClick={() => onClick(emotion_id)}
+      className={[
+        "EmotionItem",
+        isSelected ? `EmotionItem_on_${emotion_id}` : `EmotionItem_off`,
+      ].join(" ")}
+    >
+      <img src={emotion_img} alt="emotion" />
+      <span>{emotion_descript}</span>
+    </div>
+  );
+};
+
+export default EmotionItem;
